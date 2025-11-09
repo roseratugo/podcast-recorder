21b2c72645ef0fe990e79a11d38a8d3acd0dc099
Ugo ROSERAT <97634774+roseratugo@users.noreply.github.com>
2025-11-09 16:34:04 +0100

    Revert "✨ feat(packages): add shared packages for proto and ui"
    This reverts commit 53909bb1e92ab462b5f96a232d5ce4dd12a6a771.

--- packages/proto/src/types/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './room';
-export * from './participant';
-export * from './recording';
-export * from './webrtc';
--- packages/ui/src/index.ts
+++
@@ -1,9 +0,0 @@
-export { Button } from './components/Button';
-export { Input } from './components/Input';
-export { Card } from './components/Card';
-export { Avatar } from './components/Avatar';
-export { VideoTile } from './components/VideoTile';
-
-export { useMediaDevices } from './hooks/useMediaDevices';
-
-export { cn } from './utils/cn';
